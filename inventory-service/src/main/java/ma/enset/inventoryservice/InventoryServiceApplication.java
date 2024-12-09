2e4021b3a2b2f132cdbd9e8cf7a0d19ec5187b73
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-12-09 18:10:05 +0100

    inventoryService

---
+++ inventory-service/src/main/java/ma/enset/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,30 @@
+package ma.enset.inventoryservice;
+
+import ma.enset.inventoryservice.entities.Product;
+import ma.enset.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository){
+        return args -> {
+            Stream.of("Computer", "Printer", "Smartphone", "Tablet").forEach(name -> {
+                productRepository.save(Product.builder().name(name).price(Math.random()*1000).quantity((int)(Math.random()*10)).build());
+            });
+        productRepository.findAll().forEach(System.out::println);
+        };
+
+    }
+
+}
